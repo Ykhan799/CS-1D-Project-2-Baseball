ea62972680eeac4d3528bdc1d1e7f089659d9330
CoolBoy0064 <70240755+CoolBoy0064@users.noreply.github.com>
2021-04-21 15:41:38 -0700

    FileRead and expand database
    includes the database and functions to initialize and expand the database from files

---
+++ Baseball/DataBase.h
@@ -0,0 +1,136 @@
+/*
+ * AUTHOR: Liam Knight
+ * CLASS: CS1D
+ * SECTION: Data Base and file reading
+ */
+
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+#ifndef DATABASE_H_
+#define DATABASE_H_
+
+struct souvenir
+{
+	string souvenir;
+	double price;
+};
+
+class souvenirList
+{
+private:
+	souvenir souvenirs[7];
+	int noSouvenirs = 5;
+public:
+	souvenirList();
+	void addSouvenir(souvenir a)
+	{
+		//If we can still add souvenirs to the list
+		if(noSouvenirs < 7)
+		{
+			souvenirs[noSouvenirs] = a;
+			noSouvenirs++;
+		}
+	}
+	void removeSouvenir(int i)
+	{
+		//i refers to the index we want to erase
+		if(i < noSouvenirs)
+		{
+			souvenirs[i] = souvenirs[noSouvenirs];
+			souvenirs[noSouvenirs].price = 0.0;
+			souvenirs[noSouvenirs].souvenir = "a";
+		}
+	}
+	void editSouvenir(int i, string s, double p)
+	{
+		//i refers to the index we are editing
+		if(s == "a" || p == 0 || i >= 7)
+		{
+			//for the purpose of editing a souvenir, these inputs are illegal.
+			return;
+		}
+		souvenirs[i].souvenir = s;
+		souvenirs[i].price = p;
+	}
+};
+
+class anythingelse
+{
+public:
+	int key;
+	int distance;
+	string name;
+	bool operator>(const anythingelse b) const
+	{
+		return distance > b.distance;
+	}
+	bool operator<(const anythingelse b) const
+	{
+		return distance < b.distance;
+	}
+	bool operator>=(const anythingelse b) const
+	{
+		return distance >= b.distance;
+	}
+	bool operator <=(const anythingelse b) const
+	{
+		return distance <= b.distance;
+	}
+};
+
+class stadium
+{
+private:
+	int key;
+	string teamName;
+	string stadiumName;
+	int seatingCapacity;
+	string location;
+	string playingSurface;
+	string League;
+	int dateOpened;
+	int DTCF; //Distance to center Field
+	string Typology;
+	string roofType;
+	souvenirList souvenirs;
+	priority_queue<anythingelse> distances;
+public:
+	void setAll(int k, string a, string b, int c, string d, string e, string f, int g, int h, string i, string j) //set everything EXCEPT its distances and souvenirs
+	{
+		key = k;
+		teamName = a;
+		stadiumName = b;
+		seatingCapacity = c;
+		location = d;
+		playingSurface = e;
+		League = f;
+		dateOpened = g;
+		DTCF = h;
+		Typology = i;
+		roofType = j;
+	}
+	void addDistance(int k, int d, string n)
+	{
+		anythingelse newDistance;
+		newDistance.key = k;
+		newDistance.distance = d;
+		newDistance.name = n;
+		distances.push(newDistance);
+	}
+	void printName()
+	{
+		cout << stadiumName << endl;
+	}
+	priority_queue<anythingelse> getDistance()
+		{
+			return distances;
+		}
+
+};
+
+void expandData(stadium a[40], string fileName);
+void fileRead(stadium a[40], int);
+
+#endif /* DATABASE_H_ */
---
+++ Baseball/function.cpp
@@ -0,0 +1,164 @@
+/*
+ * function.cpp
+ *
+ *  Created on: Apr 21, 2021
+ *      Author: liame
+ */
+
+
+#include "dataBase.h"
+#include <bits/stdc++.h>
+#include <fstream>
+
+using namespace std;
+
+souvenirList::souvenirList()
+{
+	souvenirs[0].souvenir = "Baseball Cap";
+	souvenirs[0].price = 19.99;
+	souvenirs[1].souvenir = "Baseball Bat";
+	souvenirs[1].price = 89.39;
+	souvenirs[2].souvenir = "Team Pennant";
+	souvenirs[2].price = 17.99;
+	souvenirs[3].souvenir = "Autographed Baseball";
+	souvenirs[3].price = 29.99;
+	souvenirs[4].souvenir = "Team Jersey";
+	souvenirs[4].price = 199.99;
+}
+
+//This function takes in a array of stadiums and fills them with text input
+/************************************************************************************************************
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! *
+ ************************************************************************************************************
+ * This function has 4 assumptions:
+ * 		the array is currently empty
+ * 		the count given is EXACTLY EQUAL to the number of items in the file
+ * 		All of the input files have information for at least one stadium
+ * 		All of the input files are formated correctly
+ * 	In addition this function requires a text file named "Stadiums.txt"
+ *		And a text file named Distances.txt
+ *
+ *************************************************************************************************************/
+void fileRead(stadium a[40], int loops)
+{
+	int key;
+	int key2;
+	int distance;
+	string teamName;
+	string stadiumName;
+	int seatingCapacity;
+	string location;
+	string playingSurface;
+	string League;
+	int dateOpened;
+	int DTCF; //Distance to center Field
+	string Typology;
+	string roofType;
+	fstream infile;
+	infile.open("Stadiums.txt");
+
+	for(int i = 1; i <= loops; i++)
+	{
+		infile >> key;
+		infile.ignore(10000, '\n');
+		getline(infile, teamName);
+		getline(infile, stadiumName);
+		infile >> seatingCapacity;
+		infile.ignore(1000, '\n');
+		getline(infile, location);
+		getline(infile, playingSurface);
+		getline(infile, League);
+		infile >> dateOpened;
+		infile.ignore(1000, '\n');
+		infile >> DTCF;
+		infile.ignore(1000, '\n');
+		getline(infile, Typology);
+		getline(infile, roofType);
+
+		a[i].setAll(key, teamName, stadiumName, seatingCapacity, location, playingSurface, League, dateOpened, DTCF, Typology, roofType);
+	}
+
+	infile.close();
+	infile.open("Distances.txt");
+	stadiumName = "";
+
+
+	for(int i = 1; i <= loops; i++)
+	{
+		infile >> key;
+		infile.ignore(1000, '\n');
+		while(getline(infile, stadiumName))
+		{
+			if(stadiumName == "")
+			{
+				break;
+			}
+			infile >> key2;
+			infile.ignore(1000, '\n');
+			infile >> distance;
+			infile.ignore(1000, '\n');
+			a[i].addDistance(key2, distance, stadiumName);
+		}
+	}
+}
+
+//This function takes in a array of stadiums and fills them with text input from a given fileName
+/************************************************************************************************************
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! *
+ ************************************************************************************************************
+ * This function has 4 assumptions:
+ * 		fileName is a string that accuratley describes a text file
+ * 		the text file that fileName describes has all of the neccesary input
+ * 		The input file is formated correctly
+ * 		The key given in the text file is not equal to any key currently in the list
+ * 			If the previous assumption isnt met, the info at the key value will be replaced
+ *
+ *************************************************************************************************************/
+void expandData(stadium a[40], string fileName)
+{
+	int key;
+	int key2;
+	int distance;
+	string teamName;
+	string stadiumName;
+	int seatingCapacity;
+	string location;
+	string playingSurface;
+	string League;
+	int dateOpened;
+	int DTCF; //Distance to center Field
+	string Typology;
+	string roofType;
+	fstream infile;
+	infile.open(fileName);
+
+		infile >> key;
+		infile.ignore(10000, '\n');
+		getline(infile, teamName);
+		getline(infile, stadiumName);
+		infile >> seatingCapacity;
+		infile.ignore(1000, '\n');
+		getline(infile, location);
+		getline(infile, playingSurface);
+		getline(infile, League);
+		infile >> dateOpened;
+		infile.ignore(1000, '\n');
+		infile >> DTCF;
+		infile.ignore(1000, '\n');
+		getline(infile, Typology);
+		getline(infile, roofType);
+
+		a[key].setAll(key, teamName, stadiumName, seatingCapacity, location, playingSurface, League, dateOpened, DTCF, Typology, roofType);
+		while(getline(infile, stadiumName))
+		{
+			if(stadiumName == "")
+			{
+				break;
+			}
+			infile >> key2;
+			infile.ignore(1000, '\n');
+			infile >> distance;
+			infile.ignore(1000, '\n');
+			a[key].addDistance(key2, distance, stadiumName);
+		}
+}
